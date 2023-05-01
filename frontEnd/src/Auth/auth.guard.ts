2324e53fc55c3632ae010e54b0cb2e8473d6f7fa
Matvey <mtvy44@gmail.com>
2023-05-01 22:00:32 +0300

    auth.guard code cleanup

--- frontEnd/src/Auth/auth.guard.ts
+++ frontEnd/src/Auth/auth.guard.ts
@@ -1,31 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-//the whole purpise of this guard to check is the user is validated before rendering the page
+//the whole purpose of this guard to check is the user is validated before rendering the page
 export class AuthGuard implements CanActivate {
 
-  isAuthenticated = false
-  isValid = false
   
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService) {}
 
-  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-
-    await this.authService.getIsAuthenticated().subscribe((isAuthenticated) => {
-      this.isAuthenticated = isAuthenticated;
-    });
-    (await this.authService.getIsValid()).subscribe((isValid) => {
-      this.isValid=isValid
-    });
-    
-    if (this.isAuthenticated && this.isValid) 
-      return true;
-    
-    this.router.navigate(['/login']);
-    return false;
+  async canActivate(): Promise<boolean> {
+    console.log("\x1b[0;32m"+"checking authenticity with Auth.guard.ts" )
+    //.toPromise() is departed
+    //firstValueFrom() is used to get the first emit(or the first valve)
+    const isAuth= await firstValueFrom(this.authService.getIsAuthenticated())
+    console.log("Authenticated is ",isAuth)
+    return isAuth;
   }
 }
